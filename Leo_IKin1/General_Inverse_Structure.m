7ee902587bf654b0196e328ea04e09b4216f51b0
Leonie <leonie.elbel@online.de>
2025-01-08 14:41:12 +0100

    generated a rough structure of how to calculate inverse kinematics in general

--- Leo_IKin1/General_Inverse_Structure.m
+++ Leo_IKin1/General_Inverse_Structure.m
@@ -1,1 +1,18 @@
 %General structure for inverse kinematic
+
+%define final position and orientation of Endeffector
+%target_pos = [x, y, z];
+%target_rot = R; %rotational matrix
+
+%define starting joint configuration
+%q0 = [0 0 0 0 0 0];
+
+%create Robot
+%L1 = ...
+%...
+%L6 = ...
+
+
+%use numerical method to find the joint configuration
+%q_target = Robot.ikine(...);
+
