5227ee03a79c66b3aeca2f9d348b19d5cbca843d
englidepyc <dlombardo.dev@gmail.com>
2025-01-14 15:30:17 +0100

    Updating and printing the singlar values as well

--- Our_bot_ellipsoid.m
+++ Our_bot_ellipsoid.m
@@ -1,10 +1,12 @@
-clear; clc;
+clear; close;
 
 robot = create_bot();
 
 % Define a configuration for the robot
-q_1 = [-pi,pi/6,pi/3,-pi/4,pi/6,-pi/2];
-q = q_1;
+q_rotational_singularity = [0,0,0,0,0,0];
+q_boundary_singularity = [0,pi/2,pi/2,0,0,0]; %translational singularity
+q_nonsingular = [-pi,pi/6,pi/3,-pi/4,pi/6,-pi/2];
+q = q_nonsingular;
 
 %compute the end effector position
 Pe = transl(robot.fkine(q));
@@ -24,21 +26,31 @@ Wv = Jv * Jv';
 [Uv, Dv] = eig(Wv);
 [Uo, Do] = eig(Wo);
 
-%Display the w(q)
-w_q = sqrt(det(J*(J.'))); fprintf("The manipulability index w(q) is %d \n", w_q);
+%Display values 
+w_q = sqrt(det(J*(J.')));
+sv_x = sqrt(Dv(1,1)); sv_y = sqrt(Dv(2,2)); sv_z = sqrt(Dv(3,3));
+so_x = sqrt(Do(1,1)); so_y = sqrt(Do(2,2)); so_z = sqrt(Do(3,3));
+
+fprintf("The manipulability index w(q) is: %d\n",w_q);
+fprintf("The singular values for σ₁ σ₂ σ₃ in translation are: %d, %d, %d\n",sv_x,sv_y,sv_z);
+fprintf("The singular values for σ₁ σ₂ σ₃ in translation are: %d, %d, %d\n",so_x,so_y,so_z);
+
+
+
 
 % Plot the robot and the ellipsoid
 figure;
 hold on;
 grid on;
 axis equal;
+zlim([-20, 35]);
 
 % Plot the robot
-robot.plot(q);
+robot.plot(q,'workspace',[-30,30,-20,20,-20,20]);
 
 %Plot the ellipsoid
-create_and_plot_man_ellipsoid(Wv,Pe,'r');
-create_and_plot_man_ellipsoid(Wo,Pe,'g');
+create_and_plot_man_ellipsoid(Wv,Pe,'y');
+create_and_plot_man_ellipsoid(Wo,Pe,'b');
 
 
 % Label axes
@@ -46,4 +58,6 @@ xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('Z-axis');
 title('Manipulability Ellipsoid of a 6-DOF Robot Arm');
+
+
 hold off;
